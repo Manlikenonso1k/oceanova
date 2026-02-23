3b1e27a515c1d51b4f31bfab211290743c815711
manlikenonso1k <victorynonso9@gmail.com>
2026-02-23 13:22:07 +0100

    fixing procurment login issue 1st try

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles {
@@ -55,6 +57,18 @@ class User extends Authenticatable
     public function inventoryLogs(): HasMany
     {
         return $this->hasMany(InventoryLog::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->hasAnyRole([
+            'super_admin',
+            'admin',
+            'procurement_officer',
+            'kitchen_manager',
+            'general_order_person',
+            'barman',
+        ]);
     }
 
     public function hasRole($role, ?string $guard = null): bool
