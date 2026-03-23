c09d326cf52385422c05af467c18afe039902bd8
Manlikenonso1k <victorynonso9@gmail.com>
2026-03-23 15:55:46 +0100

    fixing buton

--- app/Filament/Resources/DailyStockResource/Pages/ListDailyStocks.php
+++ app/Filament/Resources/DailyStockResource/Pages/ListDailyStocks.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\DailyStockResource\Pages;
 
 use App\Filament\Resources\DailyStockResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListDailyStocks extends ListRecords
 {
     protected static string $resource = DailyStockResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
