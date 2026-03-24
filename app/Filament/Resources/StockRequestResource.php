6af2f42bfcdb237387ee13b25d72f7f1bc39f34a
Manlikenonso1k <victorynonso9@gmail.com>
2026-03-24 10:58:10 +0100

    added role based requests

--- app/Filament/Resources/StockRequestResource.php
+++ app/Filament/Resources/StockRequestResource.php
@@ -30,9 +30,23 @@ class StockRequestResource extends Resource
         return $form
             ->columns(1)
             ->schema([
-                Forms\Components\TextInput::make('item_name')
-                    ->required()
-                    ->maxLength(255),
+                Forms\Components\Select::make('item_name')
+                    ->label('Item')
+                    ->options(function () {
+                        $user = \Illuminate\Support\Facades\Auth::user();
+                        $query = \App\Models\Ingredient::query();
+                        if ($user && method_exists($user, 'hasAnyRole')) {
+                            if ($user->hasAnyRole(['barman', 'bar_manager'])) {
+                                $query->where('category', 'Bar');
+                            } elseif ($user->hasAnyRole(['chef', 'kitchen', 'kitchen_manager'])) {
+                                $query->where('category', 'Kitchen');
+                            }
+                        }
+
+                        return $query->orderBy('name')->pluck('name', 'name');
+                    })
+                    ->searchable()
+                    ->required(),
 
                 Forms\Components\TextInput::make('quantity')
                     ->numeric()
