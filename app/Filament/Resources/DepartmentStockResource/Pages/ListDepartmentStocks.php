6a49534516ccb0e7e445e879689ec64c45bca96a
Manlikenonso1k <victorynonso9@gmail.com>
2026-03-24 11:54:43 +0100

    fixing population bug and syntax error

--- app/Filament/Resources/DepartmentStockResource/Pages/ListDepartmentStocks.php
+++ app/Filament/Resources/DepartmentStockResource/Pages/ListDepartmentStocks.php
@@ -17,19 +17,3 @@ class ListDepartmentStocks extends ListRecords
         ];
     }
 }
-<?php
-
-namespace App\Filament\Resources\DepartmentStockResource\Pages;
-
-use App\Filament\Resources\DepartmentStockResource;
-use Filament\Resources\Pages\ListRecords;
-
-class ListDepartmentStocks extends ListRecords
-{
-    protected static string $resource = DepartmentStockResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [];
-    }
-}
